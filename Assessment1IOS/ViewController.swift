e4a4e936f988e2c845ba8f40221a58a6ffa9a902
Briley Barron <brileybarron@MacBook-Pro-101.local>
2018-10-17 12:42:32 -0500

    Finished althpugh the UI doesn't look very cool

--- Assessment1IOS/ViewController.swift
+++ Assessment1IOS/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var convertedLabel: UILabel!
+    
+    @IBOutlet weak var TextField: UITextField!
+    
+    @IBAction func DoneButtonTapped(_ sender: Any) {
+        convertedLabel.text = TextField.text
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
