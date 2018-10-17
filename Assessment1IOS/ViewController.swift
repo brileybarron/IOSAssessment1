671cfcdd4ceb24296d6f7ff06fdb896286d53d88
Briley Barron <brileybarron@MacBook-Pro-101.local>
2018-10-17 12:31:36 -0500

    started project

---
+++ Assessment1IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assessment1IOS
+//
+//  Created by Briley Barron on 10/17/18.
+//  Copyright © 2018 Briley Barron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
